b029c3f107bf3b5ec5eef001d512a10782e5d11f
yrime <yrime@main.ru>
2021-11-26 13:50:50 +0300

    modify terr.c

--- terr.c
+++ terr.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <cstdlib>
 
+
+double fact(double n) {
+    if (n <= 0) return 1;
+    else return n * fact(n - 1);
+}
+
 int main(){
 	printf("f");
 	char *b = (char*)malloc(5);
 	char *f = (char*)malloc(5);
-	b[0] = 0;
+	b[0] = 52;
 	f[0] = 0;
 	free(b);
 	free(f);
 	free(b);
+		printf("fact(5) = %g\n", fact(5));
+		printf("fact(10) = %g\n", fact(10));
+		printf("fact(100) = %g\n", fact(100));
+		printf("fact(1000) = %g\n", fact(1000));
+		printf("fact(1000000) = %g\n", fact(1000000));
 	printf("b %c", b[0]);
 	return 0;
 }
